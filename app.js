b09ded47e9897ead4ac537c43560688a48aa3f4b
品牌营销业务-JAVA-陈耀武 <jack.chen@yolipai.com>
2017-02-02 21:35:40 +0800

    user page finish

--- app.js
+++ app.js
@@ -1,31 +1,114 @@
 //app.js
 App({
   onLaunch: function () {
-    //调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-  },
-  getUserInfo:function(cb){
-    var that = this
-    if(this.globalData.userInfo){
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
-      //调用登录接口
-      wx.login({
-        success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
-            }
-          })
-        }
-      })
+    //调用API从本地缓存中获取token数据
+    var token = wx.getStorageSync('token') || "";
+    console.info("从本地缓存中获取token数据");
+
+    if (token == null || token == "") {
+      var that = this;
+      console.info("token is null");
+      this.login();
+    //token不为空，则根据token查询用户信息保存到全局数据里面
+    } else {
+      console.info("token is not null");
+      this.queryUserInfo(token);
     }
   },
-  globalData:{
-    userInfo:null,
-    backend_url : "http://127.0.0.1:8360"
+
+  //获取用户信息
+  getUserInfo: function (cb) {
+
+    //如果全局用户信息存在，则直接设置数据
+    if (this.globalData.userInfo) {
+       console.info("全局用户信息存在，则直接设置数据")
+       typeof cb == "function" && cb(this.globalData.userInfo)
+
+      //不存在则调用登录接口
+    } else {
+      console.info("本地全局用户数据不存在，调用登录接口")
+      this.login(cb);
+    }
+  },
+
+
+  //登录接口,登录成功后设置，全局用户信息
+  login: function (cb) {
+    var that = this;
+
+    wx.login({
+      //获取授权码code
+      success: function (e) {
+        console.info("授权码code")
+        console.info(e.code)
+        //获取用户信息接口 
+        wx.getUserInfo({
+          success: function (res) {
+            that.globalData.userInfo = res.userInfo;
+            console.info(res.userInfo)
+            //调用后端登录接口 
+            wx.request({
+              url: that.globalData.backend_url + '/user/login',
+              data: {
+                code: e.code,
+                nickname: res.userInfo.nickName,
+                head_img: res.userInfo.avatarUrl
+
+              },
+              //登录成功，则保存token到本地
+              success: function (res) {
+                
+                if (res.data.errno == 0) {
+                  console.info("登录成功，则保存token到本地 ")
+                  wx.setStorageSync('token', res.data.data)
+                  that.queryUserInfo(res.data.data);
+
+                }
+              }
+            })
+
+            typeof cb == "function" && cb(that.globalData.userInfo)
+          }
+        })
+      }
+    })
+  },
+
+
+  //根据token获取用户信息，token即是登录凭证
+  queryUserInfo:function(token){
+    var that = this;
+
+     if(token!="" && token!=null){
+
+         wx.request({
+              url: that.globalData.backend_url + '/user/query',
+              data: { token:token },
+              method:'POST',
+              //登录成功，则保存token到本地
+              success: function (res) {
+                
+                console.info(res.data.errno)
+                if (res.data.errno == 0) {
+                  console.info("数据库获取用户信息成功，保存到全局数据 ")
+                  var result = res.data.data;
+                  var user = {};
+                  user.nickName = result.nickname ;
+                  user.avatarUrl = result.head_img ;
+                  user.age = result.age;
+                  user.phone = result.phone; 
+                  //设置全局用户数据
+                  that.globalData.userInfo = user;  
+                }
+              }
+            })
+     }
+  },
+
+
+  //全局数据
+  globalData: {
+    userInfo: null,
+    backend_url: "http://127.0.0.1:8360"
   }
 })
--- pages/user/user.js
+++ pages/user/user.js
@@ -3,16 +3,43 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {}
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  formSubmit: function(e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+      let param = e.detail.value;
+      param.token = wx.getStorageSync('token') || "";
+      //吐司弹窗提示
+       wx.showToast({
+      title: '修改中',
+      icon: 'loading',
+      duration: 2000 })
+
+        //发送网络请求，获取数据
+        var that = this
+        //更新用户信息
+        wx.request({
+            url: app.globalData.backend_url + '/user/bindinfo',
+            data: e.detail.value,
+            method:'POST',
+            success: function (res) {
+              console.info(res)
+               wx.showToast({
+              title: '修改成功',
+              icon: 'success',
+              duration: 2000 })
+
+            }
+        })
+
   },
-  onLoad: function () {
+
+
+
+
+
+  onLoad: function(){
     console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
